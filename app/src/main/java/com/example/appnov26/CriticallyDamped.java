f398b5d7191195262e5d38c3e67af0047d5c79f5
gonzalese <egonz@bu.edu>
2017-12-06 03:30:56 -0500

    Rough Calculators
    Brought to you by jess

--- app/src/main/java/com/example/appnov26/CriticallyDamped.java
+++ app/src/main/java/com/example/appnov26/CriticallyDamped.java
@@ -1,18 +1,63 @@
 package com.example.appnov26;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.text.TextUtils.isEmpty;
+import static java.lang.Math.sqrt;
 
 public class CriticallyDamped extends AppCompatActivity {
 
+    private EditText cd_input1;
+    private EditText cd_input2;
+    private TextView cd_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_critically_damped);
-        Intent intent0 = getIntent();
+        cd_input1 = (EditText) findViewById(R.id.cd_input1);
+        cd_input2 = (EditText) findViewById(R.id.cd_input2);
+
+        Button vd_calculate = (Button) findViewById(R.id.cd_calculate);
+        cd_result = (TextView) findViewById(R.id.cd_result);
+        vd_calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeCDCalculations();
+            }
+        });
+    }
+
+    private void makeCDCalculations() {
+        double R;
+        double L;
+        double C;
+
+        boolean isnotfilled1 = isEmpty(cd_input1.getText().toString().trim());
+        boolean isnotfilled2 = isEmpty(cd_input2.getText().toString().trim());
+        int nf1 = (isnotfilled1) ? 1 : 0;
+        int nf2 = (isnotfilled2) ? 1 : 0;
+
+        if (nf1 != 0) {
+            cd_result.setText("Inductance Needed");
+        } else if (nf2 != 0) {
+            cd_result.setText("Capacitance Needed");
+        } else {
+            L = Double.valueOf(cd_input1.getText().toString());
+            C = Double.valueOf(cd_input2.getText().toString());
+            if (L == 0 || C == 0) {
+                cd_result.setText("Critically damped R= 0.0 Ohms");
+            } else {
+                R = sqrt((4 * L) / C);
+                cd_result.setText("Critically damped R = " + R + " Ohms");
+            }
+        }
+
 
     }
 
--- app/src/main/java/com/example/appnov26/VoltageDivider.java
+++ app/src/main/java/com/example/appnov26/VoltageDivider.java
@@ -1,22 +1,74 @@
 package com.example.appnov26;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.text.TextUtils.isEmpty;
 
 public class VoltageDivider extends AppCompatActivity {
-
+    private EditText vd_input1;
+    private EditText vd_input2;
+    private EditText vd_input3;
+    private TextView vd_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voltage_divider);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        vd_input1 = (EditText) findViewById(R.id.vd_input1);
+        vd_input2 = (EditText) findViewById(R.id.vd_input2);
+        vd_input3 = (EditText) findViewById(R.id.vd_input3);
+
+        Button vd_calculate=(Button) findViewById(R.id.vd_calculate);
+        vd_result = (TextView) findViewById(R.id.vd_result);
+        vd_calculate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){makeVDCalculations();}
+        });
+    }
+
+    private void makeVDCalculations() {
+        double R1;
+        double R2;
+        double V;
+        double V1;
+        double V2;
+
+
+        boolean isnotfilled1 = isEmpty(vd_input1.getText().toString().trim());
+        boolean isnotfilled2 = isEmpty(vd_input2.getText().toString().trim());
+        boolean isnotfilled3 = isEmpty(vd_input3.getText().toString().trim());
+        int nf1 = (isnotfilled1) ? 1 : 0;
+        int nf2 = (isnotfilled2) ? 1 : 0;
+        int nf3 = (isnotfilled3) ? 1 : 0;
+
+
+        if (nf1 != 0) {
+            vd_result.setText("Resistance 1 Needed");
+        } else if (nf2 != 0) {
+            vd_result.setText("Resistance 2 Needed");
+        } else if (nf3 != 0) {
+            vd_result.setText("Voltage Value Needed");
+        } else {
+            R1 = Double.valueOf(vd_input1.getText().toString());
+            R2 = Double.valueOf(vd_input2.getText().toString());
+            V = Double.valueOf(vd_input3.getText().toString());
+            double denom = R1 + R2;
+            if (R1 == 0) {
+                vd_result.setText("Vout = " + V + " Volts");
+            } else if (R2 == 0) {
+                V1 = (R1 / denom) * V;
+                V2 = 0;
+                vd_result.setText("Vout = " + V2 + " Volts");
+            } else {
+                V2 = (R2 / denom) * V;
+                vd_result.setText("Vout " + V2 + "Volts");
+            }
+        }
 
 
     }
-
 }
